a6e184a9389fac60611518a4fb183a23d059e4a8
tianhuizhou <tianhui@hawaii.edu>
2021-05-18 23:23:52 -1000

    fixed

--- app.py
+++ app.py
@@ -9,9 +9,12 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
+
+app.config['DEBUG'] = True
+
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql://localhost/tianhui')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['PROPAGATE_EXCEPTIONS'] = True
+
 app.secret_key = 'jose'
 api = Api(app)
 
