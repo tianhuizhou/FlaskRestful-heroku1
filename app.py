8b9c6a349604765a717cf8ebb475c12c80c1875e
tianhuizhou <tianhui@hawaii.edu>
2021-05-18 22:56:34 -1000

    update

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
-from db import db
+
 from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
@@ -25,9 +25,12 @@ api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
+    from db import db
     db.init_app(app)
+
     if app.config['DEBUG']:
         @app.before_first_request
         def create_tables():
             db.create_all()
-    app.run(port=5000, debug=True)
+
+    app.run(port=5000)
