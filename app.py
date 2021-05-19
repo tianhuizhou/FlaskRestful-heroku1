6f36d56daaefbd724497ec3391925a3969f9da0f
tianhuizhou <tianhui@hawaii.edu>
2021-05-18 23:34:03 -1000

    fixed

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql://localhost/tianhui')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql://localhost/tianhuizhou')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app.secret_key = 'jose'
